cbf48fa654ce2eff31bdcc21ddfcb04673d6d165
Ethan <ethan@venari.co.nz>
2022-01-28 18:57:47 +1300

    move work from main

--- scripts/log.py
+++ scripts/log.py
@@ -4,14 +4,36 @@ from pijuice import PiJuice
 import time
 import psutil
 import os
+import json
+try:
+    import requests
+except ImportError:
+    print('requests module not found, try "python3 -m pip install requests"')
 
-pj = PiJuice(1, 0x14) #?
 
+config = json.load(open(os.path.dirname(os.path.realpath(__file__)) + '/config.json'))
 log = f'{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}, {pj.status.GetChargeLevel()["data"]}, {pj.status.GetBatteryTemperature()["data"]}, {pj.status.GetStatus()["data"]["battery"]}, {time.time() - psutil.boot_time()}\n'
 
-outFile = os.path.dirname(os.path.realpath(__file__)) + '/../output/log.csv'
+if config['logToFile']:
+    pj = PiJuice(1, 0x14) #?
 
-with open(outFile, 'a') as f:
-    f.write(log)
+    log = f'{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}, {pj.status.GetChargeLevel()["data"]}, {pj.status.GetBatteryTemperature()["data"]}\n'
 
-print('Logging complete')
+    outFile = os.path.dirname(os.path.realpath(__file__)) + '/../output/log.csv'
+
+    with open(outFile, 'a') as f:
+        f.write(log)
+
+    print('Logged to file.')
+
+if config['logToAPI']:
+
+    api_data = {
+            'time': time.time(),
+            'charge': pj.status.GetChargeLevel(),
+            'temp': pj.status.GetBatteryTemperature()
+        }
+
+    requests.post(config['apiUrl'], json=api_data)
+
+    print('Logged to API.')
