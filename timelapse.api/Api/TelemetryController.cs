82e1ff6596c5fcf841bb2ec4d3438330c973fb33
Leigh Hunt <leigh@venari.co.nz>
2022-01-28 20:50:31 +1300

    Further tweak troubleshooting

--- timelapse.api/Api/TelemetryController.cs
+++ timelapse.api/Api/TelemetryController.cs
@@ -28,7 +28,7 @@ namespace timelapse.api{
             _logger.LogInformation("In Telemenrty Post");
             _logger.LogInformation("DeviceId: " + model.DeviceId);
             _logger.LogInformation("Timestamp: " + model.Timestamp);
-            
+
             Telemetry telemetry = new Telemetry(){
                 DeviceId = model.DeviceId,
                 Timestamp = model.Timestamp==DateTime.MinValue?DateTime.Now.ToUniversalTime():model.Timestamp,
@@ -37,7 +37,7 @@ namespace timelapse.api{
                 DiskSpaceFree = model.DiskSpaceFree,
                 UptimeSeconds = model.UptimeSeconds
             };
-            _logger.LogInformation("Add Telemetry");
+            _logger.LogInformation("Add Telemetry BLAH");
             _appDbContext.Telemetry.Add(telemetry);
             _appDbContext.SaveChanges();
             return telemetry;
