c6e16dd0ee716b0688dfbda8ba8fa2118d986597
Leigh Hunt <leigh@venari.co.nz>
2022-01-28 16:05:24 +1300

    Azure Storage stuff

--- azure-helpers/create-timelapse-dev.sh
+++ azure-helpers/create-timelapse-dev.sh
@@ -8,48 +8,55 @@ AZURE_APP_PLAN_NAME=timelapse
 AZURE_LOCATION=australiasoutheast
 DB_SERVER=timelapse
 DB_NAME=timelapse-dev
-VNET_NAME=timelapse
-PRIVATE_DNS_ZONE_PREFIX=timelapse
-SUBNET_NAME=timelapse
-SUBNET_PREFIXES=10.0.0.0/24
+# VNET_NAME=timelapse
+# PRIVATE_DNS_ZONE_PREFIX=timelapse
+# SUBNET_NAME=timelapse
+# SUBNET_PREFIXES=10.0.0.0/24
+STORAGE_ACCOUNT_NAME=venaritimelapse
+STORAGE_CONTAINER_NAME=timelapse
 # KEYVAULT_NAME=timelapse-dev
 
+echo Determining IP Address...
+IP_ADDRESS=$(curl ipecho.net/plain)
+
+
 echo "Checking if app service plan $AZURE_APP_PLAN_NAME already exists...."
-MATCHING_APP_PLAN_NAME_COUNT=$(az appservice plan list --query "[?name=='$AZURE_APP_PLAN_NAME'].{name:name}.length(@)")
-if [ $MATCHING_APP_PLAN_NAME_COUNT = 0 ]
+MATCHING_COUNT=$(az appservice plan list --query "[?name=='$AZURE_APP_PLAN_NAME'].{name:name}.length(@)")
+if [ $MATCHING_COUNT = 0 ]
 then
     echo "Creating app service plan $AZURE_APP_PLAN_NAME..."
     az appservice plan create --name $AZURE_APP_PLAN_NAME --resource-group $AZURE_RESOURCE_GROUP --sku F1 --is-linux --location $AZURE_LOCATION
+    echo "✅"
 else
-    echo "App Service Plan $AZURE_APP_PLAN_NAME already exists."
+    echo "✔️"
 fi
 
 
-echo "Checking if VNET $VNET_NAME already exists...."
-MATCHING_VNET_NAME_COUNT=$(az network vnet list --query "[?name=='$VNET_NAME'].{name:name}.length(@)")
-if [ $MATCHING_VNET_NAME_COUNT = 0 ]
-then
-    echo "Creating VNET $VNET_NAME..."
-    az network vnet create --name $VNET_NAME --resource-group $AZURE_RESOURCE_GROUP  --location $AZURE_LOCATION
-else
-    echo "VNET $VNET_NAME already exists."
-fi
+# echo "Checking if VNET $VNET_NAME already exists...."
+# MATCHING_COUNT=$(az network vnet list --query "[?name=='$VNET_NAME'].{name:name}.length(@)")
+# if [ $MATCHING_COUNT = 0 ]
+# then
+#     echo "Creating VNET $VNET_NAME..."
+#     az network vnet create --name $VNET_NAME --resource-group $AZURE_RESOURCE_GROUP  --location $AZURE_LOCATION
+# else
+#     echo "VNET $VNET_NAME already exists."
+# fi
 
-echo "Checking if VNET subnet $SUBNET_NAME already exists...."
-MATCHING_SUBNET_NAME_COUNT=$(az network vnet subnet list --vnet-name $VNET_NAME --resource-group $AZURE_RESOURCE_GROUP --query "[?name=='$SUBNET_NAME'].{name:name}.length(@)")
-if [ $MATCHING_VNET_NAME_COUNT = 0 ]
-then
-    echo "Creating VNET subnet $SUBNET_NAME..."
-    az network vnet subnet create  --resource-group $AZURE_RESOURCE_GROUP --vnet-name $VNET_NAME --name $SUBNET_NAME --address-prefixes $SUBNET_PREFIXES      
-else
-    echo "VNET subnet $SUBNET_NAME already exists."
-fi
+# echo "Checking if VNET subnet $SUBNET_NAME already exists...."
+# MATCHING_COUNT=$(az network vnet subnet list --vnet-name $VNET_NAME --resource-group $AZURE_RESOURCE_GROUP --query "[?name=='$SUBNET_NAME'].{name:name}.length(@)")
+# if [ $MATCHING_COUNT = 0 ]
+# then
+#     echo "Creating VNET subnet $SUBNET_NAME..."
+#     az network vnet subnet create  --resource-group $AZURE_RESOURCE_GROUP --vnet-name $VNET_NAME --name $SUBNET_NAME --address-prefixes $SUBNET_PREFIXES      
+# else
+#     echo "VNET subnet $SUBNET_NAME already exists."
+# fi
 
 
 
 echo "Checking if webapp $AZURE_APP_NAME already exists...."
-MATCHING_APP_NAME_COUNT=$(az webapp list --query "[?name=='$AZURE_APP_NAME'].{name:name}.length(@)")
-if [ $MATCHING_APP_NAME_COUNT = 0 ]
+MATCHING_COUNT=$(az webapp list --query "[?name=='$AZURE_APP_NAME'].{name:name}.length(@)")
+if [ $MATCHING_COUNT = 0 ]
 then
     echo "Creating webapp $AZURE_APP_NAME..."
     az webapp create --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP  --plan $AZURE_APP_PLAN_NAME --runtime "DOTNET|6.0" 
@@ -60,8 +67,9 @@ then
     # Command group 'webapp deployment github-actions' is in preview and under development. Reference and support levels: https://aka.ms/CLI_refstatus
     # Verified GitHub repo and branch
     # Runtime DOTNET|6.0 is not supported for GitHub Actions deployments.
+    echo "✅"
 else
-    echo "Webapp $AZURE_APP_NAME already exists."
+    echo "✔️"
 fi
 
 
@@ -85,40 +93,92 @@ ConnectionStrings__DefaultConnection=$TimelapseDev_ConnectionStrings__DefaultCon
 
 
 echo "Checking if PostgreSQL Server $DB_SERVER already exists...."
-MATCHING_DB_SERVER_NAME_COUNT=$(az postgres flexible-server list --resource-group $AZURE_RESOURCE_GROUP --query "[?name=='$DB_SERVER'].{name:name}.length(@)")
-if [ $MATCHING_DB_SERVER_NAME_COUNT = 0 ]
+MATCHING_COUNT=$(az postgres flexible-server list --resource-group $AZURE_RESOURCE_GROUP --query "[?name=='$DB_SERVER'].{name:name}.length(@)")
+if [ $MATCHING_COUNT = 0 ]
 then
-    echo Determining IP Address...
-    export IP_ADDRESS=$(curl ipecho.net/plain)
-
     echo "Creating DB Server $DB_SERVER..."
     az postgres flexible-server create --admin-user $Timelapse_DBadmin_user --admin-password $Timelapse_DBadmin_password  --name $DB_SERVER --location $AZURE_LOCATION  --resource-group $AZURE_RESOURCE_GROUP \
     --database-name $DB_NAME \
     --tier Burstable --sku-name Standard_B1ms  --storage-size 32  --version 13
     # --public-access $IP_ADDRESS
     # --vnet $VNET_NAME --private-dns-zone $PRIVATE_DNS_ZONE_PREFIX.private.postgres.database.azure.com --subnet $SUBNET_NAME
+    echo "✅"
 else
-    echo "PostgreSQL Server $DB_SERVER already exists."
+    echo "✔️"
 fi
 
 echo "Checking if PostgreSQL Database $DB_NAME already exists...."
-MATCHING_DB_NAME_COUNT=$(az postgres flexible-server db list --resource-group $AZURE_RESOURCE_GROUP --server-name $DB_SERVER --query "[?name=='$DB_NAME'].{name:name}.length(@)")
-if [ $MATCHING_DB_NAME_COUNT = 0 ]
+MATCHING_COUNT=$(az postgres flexible-server db list --resource-group $AZURE_RESOURCE_GROUP --server-name $DB_SERVER --query "[?name=='$DB_NAME'].{name:name}.length(@)")
+if [ $MATCHING_COUNT = 0 ]
 then
     echo "Creating Databaase $DB_NAME..."
     az postgres flexible-server db create --resource-group $AZURE_RESOURCE_GROUP --server-name $DB_SERVER --database-name $DB_NAME
-
+    echo "✅"
 else
-    echo "PostgreSQL Database $DB_NAME already exists."
+    echo "✔️"
 fi
 
-echo "Updating PostgreSQL firewall rules..."
-az postgres flexible-server firewall-rule create --name $DB_SERVER --resource-group $AZURE_RESOURCE_GROUP --start-ip-address $IP_ADDRESS --end-ip-address $IP_ADDRESS --rule-name 'WEBAPP-ACCESS'
-az postgres flexible-server firewall-rule create --name $DB_SERVER --resource-group $AZURE_RESOURCE_GROUP --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0 --rule-name 'AZURE_ACCESS'
+echo "Checking PostgreSQL firewall rules..."
+MATCHING_COUNT=$(az postgres flexible-server firewall-rule list --name $DB_SERVER --resource-group $AZURE_RESOURCE_GROUP --query "[?startIpAddress=='$IP_ADDRESS'].{name:name}.length(@)")
+if [ $MATCHING_COUNT = 0 ]
+then
+    echo "Creating Firewall Rule AccessFromHome..."
+    az postgres flexible-server firewall-rule create --name $DB_SERVER --resource-group $AZURE_RESOURCE_GROUP --start-ip-address $IP_ADDRESS --rule-name 'AccessFromHome'
+    echo "✅"
+else
+    echo "✔️"
+fi
+
+
+MATCHING_COUNT=$(az postgres flexible-server firewall-rule list --name $DB_SERVER --resource-group $AZURE_RESOURCE_GROUP --query "[?startIpAddress=='0.0.0.0'].{name:name}.length(@)")
+if [ $MATCHING_COUNT = 0 ]
+then
+    echo "Creating Firewall Rule AccessFromAzure..."
+    az postgres flexible-server firewall-rule create --name $DB_SERVER --resource-group $AZURE_RESOURCE_GROUP --start-ip-address 0.0.0.0 --rule-name 'AccessFromAzure'
+    echo "✅"
+else
+    echo "✔️"
+fi
 
 
 echo "Running migrations..."
 dotnet ef --project timelapse.api database update --connection "$ConnectionStrings__DefaultConnection"
+
+
+echo "Checking if Storage Account $STORAGE_ACCOUNT_NAME already exists...."
+MATCHING_COUNT=$(az storage account list --query "[?name=='$STORAGE_ACCOUNT_NAME'].{name:name}.length(@)")
+if [ $MATCHING_COUNT = 0 ]
+then
+    echo "Creating Storage Account $STORAGE_ACCOUNT_NAME..."
+    az storage account create --name $STORAGE_ACCOUNT_NAME --resource-group $AZURE_RESOURCE_GROUP --location $AZURE_LOCATION
+    echo "✅"
+else
+    echo "✔️"
+fi
+
+# STORAGE_KEY1=$(az storage account keys list --account-name $STORAGE_ACCOUNT_NAME --resource-group $AZURE_RESOURCE_GROUP --query "[?keyName=='key1'].{value:value}[0].value")
+# STORAGE_KEY2=$(az storage account keys list --account-name $STORAGE_ACCOUNT_NAME --resource-group $AZURE_RESOURCE_GROUP --query "[?keyName=='key2'].{value:value}[0].value")
+STORAGE_CONNECTION_STRING=$(az storage account show-connection-string --name $STORAGE_ACCOUNT_NAME --key primary --query "connectionString")
+
+
+echo "Checking if Storage Container $STORAGE_CONTAINER_NAME already exists...."
+MATCHING_COUNT=$(az storage container list --account-name $STORAGE_ACCOUNT_NAME --connection-string $STORAGE_CONNECTION_STRING --query "[?name=='$STORAGE_CONTAINER_NAME'].{name:name}.length(@)")
+if [ $MATCHING_COUNT = 0 ]
+then
+    echo "Creating Storage Container $STORAGE_CONTAINER_NAME..."
+    az storage container create --account-name $STORAGE_ACCOUNT_NAME --name $STORAGE_CONTAINER_NAME --connection-string $STORAGE_CONNECTION_STRING # --public-access blob
+    echo "✅"
+else
+    echo "✔️"
+fi
+
+az webapp config appsettings set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --settings STORAGE_CONNECTION_STRING=$STORAGE_CONNECTION_STRING --output none
+az webapp config appsettings set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --settings STORAGE_CONTAINER_NAME=$STORAGE_CONTAINER_NAME --output none
+dotnet user-secrets --project timelapse.api set "STORAGE_CONNECTION_STRING" $STORAGE_CONNECTION_STRING
+# az webapp config appsettings set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --settings STORAGE_ACCOUNT_NAME=$STORAGE_ACCOUNT_NAME
+# az webapp config appsettings set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --settings STORAGE_CONTAINER_NAME=$STORAGE_CONTAINER_NAME
+# az webapp config appsettings set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --settings STORAGE_KEY1=$STORAGE_KEY1
+# az webapp config appsettings set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --settings STORAGE_KEY2=$STORAGE_KEY2
 
 
 
@@ -131,9 +191,10 @@ dotnet ef --project timelapse.api database update --connection "$ConnectionStrin
 # az postgres flexible-server firewall-rule create --name $DB_SERVER --resource-group $AZURE_RESOURCE_GROUP --start-ip-address $WEBAPP_IPADDRESS --end-ip-address $WEBAPP_IPADDRESS --rule-name 'WEBAPP-ACCESS'
 
 
-# az webapp config connection-string set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --connection-string-type PostgreSQL --settings DefaultConnection="$ConnectionStrings__DefaultConnection"
-az webapp config connection-string set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --connection-string-type SQLAzure --settings DefaultConnection="$ConnectionStrings__DefaultConnection"
-az webapp config appsettings set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --settings ASPNETCORE_ENVIRONMENT=Development
+# az webapp config connection-string set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --connection-string-type PostgreSQL --settings DefaultConnection="$ConnectionStrings__DefaultConnection" --output none
+az webapp config connection-string set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --connection-string-type SQLAzure --settings DefaultConnection="$ConnectionStrings__DefaultConnection" --output none
+# az webapp config appsettings set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --settings ASPNETCORE_ENVIRONMENT=Development --output none
+az webapp config appsettings set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --settings ASPNETCORE_ENVIRONMENT=Production --output none
 
 # az webapp config connection-string set --name $AZURE_APP_NAME --resource-group $AZURE_RESOURCE_GROUP --connection-string-type PostgreSQL --settings DefaultConnection='Host=timelapse.postgres.database.azure.com;Port=5432;User ID={DBUsername};Password={DBPassword};Database=timelapse'
 # az keyvault secret set --vault-name "$KEYVAULT_NAME" --name "DBUsername" --value "api"
---
+++ timelapse.api/Helpers/StorageHelper.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace timelapse.api.Helpers
+{
+    class StorageHelper{
+
+        private IConfiguration config;
+        private ILogger _logger { get; }
+
+        private Azure.Storage.Blobs.BlobServiceClient blobServiceClient = null;
+        private Azure.Storage.Blobs.BlobContainerClient blobContainerClient = null;
+        private string azureStorageConnectionString = null;
+        private string azureBlobContainerName = null;
+
+        public StorageHelper(IConfiguration configuration, ILogger logger){
+            config = configuration;
+            _logger = logger;
+
+            azureStorageConnectionString = config["STORAGE_CONNECTION_STRING"];
+            azureBlobContainerName = config["STORAGE_CONTAINER_NAME"];
+
+            blobContainerClient = new Azure.Storage.Blobs.BlobContainerClient(azureStorageConnectionString, azureBlobContainerName);
+        }
+
+        public bool Download(string blobName, string localFilePath){
+            try{
+                _logger.LogDebug($"Download(\"{blobName}\")");
+                Azure.Storage.Blobs.Models.BlobDownloadInfo blobDownloadInfo = blobContainerClient.GetBlobClient(blobName).Download();
+                
+                using(FileStream fileStream = File.OpenWrite(localFilePath)){
+                    blobDownloadInfo.Content.CopyTo(fileStream);
+                }
+
+                return true;
+            }
+            catch(Exception ex){
+                _logger.LogError($"Error trying to access blob {blobName}");
+                _logger.LogError(ex.ToString());
+                throw;
+            }
+        }
+
+        public bool Upload(string blobName, string localFilePath){
+            try{
+                _logger.LogDebug($"Upload(\"{blobName}\")");
+                Azure.Storage.Blobs.BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+                using(FileStream uploadFileStream = File.OpenRead(localFilePath)){
+                    blobClient.Upload(uploadFileStream, true);
+                    uploadFileStream.Close();
+                }
+                return true;
+            }
+            catch(Exception ex){
+                _logger.LogError($"Error trying to access blob {blobName}");
+                _logger.LogError(ex.ToString());
+                throw;
+            }
+        }
+
+        public Uri GenerateSasUri(){
+            try{
+                var sasUri = blobContainerClient.GenerateSasUri(Azure.Storage.Sas.BlobContainerSasPermissions.Read, DateTimeOffset.UtcNow.AddHours(1));
+                return sasUri;
+            }
+            catch(Exception ex){
+                _logger.LogError($"Error trying to GenerateSasUri");
+                _logger.LogError(ex.ToString());
+                throw;
+            }
+        }
+    }
+}
