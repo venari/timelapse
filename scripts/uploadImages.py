03ca66024bb42362ddc5ca6462c99294ff4aeb63
Leigh Hunt <leigh@venari.co.nz>
2022-01-31 12:34:23 +1300

    api isn't complaining anymore

--- scripts/uploadImages.py
+++ scripts/uploadImages.py
@@ -7,10 +7,23 @@ localConfig = json.load(open('localConfig.json'))
 with open('./lastImage.path', 'r') as f:
     lastImagePath = f.read()
 
-data = {
-    'image': open(lastImagePath, 'rb'),
-    'deviceId': localConfig['deviceId']
+files = {
+    'File': open(lastImagePath, 'rb'),
 }
 
+data = {
+    'DeviceId': localConfig['deviceId']
+}
+
+print('data:')
+print(data)
+
 session = requests.Session()
-session.post(config['apiURL'] + 'Image', data=data)
+response = session.post(config['apiUrl'] + 'Image', files=files, data=data)
+
+print(f'Response code: {response.status_code}')
+print(f'Response text:')
+try:
+    print(json.dumps(json.loads(response.text), indent = 4))
+except json.decoder.JSONDecodeError:
+    print(response.text)
