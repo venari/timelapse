b4d25f78ec7f4feae3010a0c348c1430689774e4
Ethan <ethandhunt@gmail.com>
2022-01-31 12:46:57 +1300

    remove uploadImages.py from startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,5 @@
 pushd /home/pi/dev/timelapse/scripts
 /usr/bin/python3 test.py > test.py.out 2> test.py.err
 /usr/bin/python3 log.py > log.py.out 2> log.py.err
-/usr/bin/python3 uploadImages.py > uploadImages.py.out 2> uploadImages.py.err
 /usr/bin/git pull >> gitPull.log
 popd
