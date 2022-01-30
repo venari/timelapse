c0bc60f1bfdb906b6ec18368119390f3cb2741ce
Leigh Hunt <leigh@venari.co.nz>
2022-01-31 11:27:02 +1300

    fix

--- scripts/uploadImages.py
+++ scripts/uploadImages.py
@@ -2,13 +2,14 @@ import requests
 import json
 
 config = json.load(open('config.json'))
+localConfig = json.load(open('localConfig.json'))
 
 with open('./lastImage.path', 'r') as f:
     lastImagePath = f.read()
 
 data = {
-    'image': open(lastImagePath, 'rb')
-    'deviceId': config['deviceId']
+    'image': open(lastImagePath, 'rb'),
+    'deviceId': localConfig['deviceId']
 }
 
 session = requests.Session()
