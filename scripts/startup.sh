b18e62d1912bc5d6bc8a9530f41063c145711561
Ethan <ethandhunt@gmail.com>
2022-01-31 14:22:02 +1300

    better debugging for git pull log

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,5 +3,8 @@ rm *.err
 rm *.out
 /usr/bin/python3 test.py > test.py.out 2> test.py.err
 /usr/bin/python3 log.py > log.py.out 2> log.py.err
+date >> gitPull.log
+echo "> git pull" >> gitPull.log
 /usr/bin/git pull >> gitPull.log
+echo >> gitPull.log
 popd
