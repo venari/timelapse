9fd7eebf250da236866fd4504a07d3f4eab667ba
Ethan <ethandhunt@gmail.com>
2022-01-31 14:16:26 +1300

    remove .out and .err files before recreating them, causes files that no longer run to appear in error logs

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,6 @@
 pushd /home/pi/dev/timelapse/scripts
+rm *.err
+rm *.out
 /usr/bin/python3 test.py > test.py.out 2> test.py.err
 /usr/bin/python3 log.py > log.py.out 2> log.py.err
 /usr/bin/git pull >> gitPull.log
