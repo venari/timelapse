659a58852f883cd6f2d1668e410f287f797c0b1a
Leigh Hunt <leigh@venari.co.nz>
2022-02-01 16:13:58 +1300

    Development Connection string helpers

--- azure-helpers/connect-to-dev-db.sh
+++ azure-helpers/connect-to-dev-db.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/database-connection-strings.secret.sh
 ConnectionStrings__DefaultConnection=$TimelapseDev_ConnectionStrings__DefaultConnection
 
-dotnet user-secrets --project timelapse.api set "STORAGE_CONNECTION_STRING" $STORAGE_CONNECTION_STRING
+dotnet user-secrets --project timelapse.api set "ConnectionStrings:DefaultConnection" "$ConnectionStrings__DefaultConnection"
