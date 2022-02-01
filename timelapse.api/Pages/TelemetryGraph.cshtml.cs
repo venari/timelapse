bde790f156cd494782232bc5b193461d3adb3548
Leigh Hunt <leigh@venari.co.nz>
2022-02-01 15:36:39 +1300

    Image and Telemetry pages

---
+++ timelapse.api/Pages/TelemetryGraph.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using timelapse.core.models;
+using timelapse.infrastructure;
+using Microsoft.EntityFrameworkCore;
+using timelapse.api.Helpers;
+
+namespace timelapse.api.Pages;
+
+public class TelemetryGraphModel : PageModel
+{
+    private readonly ILogger<TelemetryGraphModel> _logger;
+    private AppDbContext _appDbContext;
+    private StorageHelper _storageHelper;
+
+    public List<Device> devices {get;}
+    public Device device {get; private set;}
+    public string SasToken {get; private set;}
+
+    public TelemetryGraphModel(ILogger<TelemetryGraphModel> logger, AppDbContext appDbContext, IConfiguration configuration)
+    {
+        _logger = logger;
+        _appDbContext = appDbContext;
+        devices = _appDbContext.Devices
+            .Include(d => d.Telemetries)
+            .Include(d => d.Images)
+            .ToList();
+        _storageHelper = new StorageHelper(configuration, logger);
+
+        var sasUri = _storageHelper.GenerateSasUri();
+        // Extract the Token from the URI
+        SasToken = sasUri.Query;
+
+        // _appDbContext.Database.EnsureCreated();
+    }
+
+    public void OnGet(int id)
+    {
+        device = devices.Find(d => d.Id == id);
+    }
+}
