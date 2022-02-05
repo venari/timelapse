cbb92cd9114335bf814e110320f01ed2f37685e1
Leigh Hunt <leigh@venari.co.nz>
2022-02-05 22:12:53 +1300

    Logging

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,7 +3,8 @@ set -x
 # rm *.out
 echo >> coreScript.py.out
 date >> coreScript.py.out
-/usr/bin/python3 coreScript.py 2& >> coreScript.py.out
+# /usr/bin/python3 coreScript.py 2& >> coreScript.py.out
+/usr/bin/python3 coreScript.py >> coreScript.py.out
 echo >> gitPull.log
 date >> gitPull.log
 echo "> git pull" >> gitPull.log
