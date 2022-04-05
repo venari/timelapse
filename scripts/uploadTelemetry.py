6f14c098d9f59d917314a16b5317575090dfcf5a
Leigh Hunt <leigh@venari.co.nz>
2022-04-05 13:25:15 +1200

    PiJuice setup notes - need to script

--- scripts/uploadTelemetry.py
+++ scripts/uploadTelemetry.py
@@ -31,6 +31,22 @@ serialNumber = getSerialNumber()
 
 def uploadTelemetry():
     warningTemp = 50
+
+    print('A')
+    print(pj.status.GetChargeLevel()['data'])
+    print(pj.status.GetBatteryTemperature()['data'])
+    print(shutil.disk_usage('/')[2] // (1024**3)) # shutil.disk_usage returns tuple of (total, used, free), converted to int gb
+    print(int(time.clock_gettime(time.CLOCK_BOOTTIME)))
+    print(str({ 'status': pj.status.GetStatus()['data'],
+                'batteryVoltage': pj.status.GetBatteryVoltage()['data'],
+                'batteryCurrent': pj.status.GetBatteryCurrent()['data'],
+                'ioVoltage': pj.status.GetIoVoltage()['data'],
+                'ioCurrent': pj.status.GetIoCurrent()['data']
+            }))
+    print(serialNumber)
+    print('Z')
+
+
     api_data = {
                 'batteryPercent': pj.status.GetChargeLevel()['data'],
                 'temperatureC': pj.status.GetBatteryTemperature()['data'],
