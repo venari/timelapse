b48c182bd491dbce9bbf753ec4916294dae794b3
Leigh Hunt <leigh@venari.co.nz>
2022-09-20 15:34:49 +1200

    configuration changes

--- scripts/coreScript.py
+++ scripts/coreScript.py
@@ -77,7 +77,7 @@ def scheduleShutdown():
 
         setAlarm = True
 
-    if datetime.datetime.now().hour >=18 or datetime.datetime.now().hour <= 7:
+    if datetime.datetime.now().hour >=20 or datetime.datetime.now().hour < 5:
         logging.info("Night time so we're scheduling shutdown")
 
         alarmObj = {
