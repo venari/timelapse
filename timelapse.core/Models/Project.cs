c9721dd8dac1ba8e4330977a4abef14c6ec46e36
Ethan <ethandhunt@gmail.com>
2023-01-24 13:37:06 +1300

    Consistent naming

--- timelapse.core/Models/Project.cs
+++ timelapse.core/Models/Project.cs
@@ -6,7 +6,7 @@ public class Project
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public int OrganisationID { get; set; }
+    public int OrganisationId { get; set; }
     [System.Text.Json.Serialization.JsonIgnore]
     public Organisation Organisation { get; set; }
     public List<DeviceProjectContract> DeviceProjectContracts { get; } = new List<DeviceProjectContract>();
