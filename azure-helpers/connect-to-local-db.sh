0a4d0e12e8695e1d7584be4b80fba50860b9edda
Leigh Hunt <leigh@venari.co.nz>
2023-01-25 16:26:37 +1300

    Script stuff to set SendAPIKey

--- azure-helpers/connect-to-local-db.sh
+++ azure-helpers/connect-to-local-db.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 dotnet user-secrets --project timelapse.api set "ConnectionStrings:DefaultConnection" "Host=localhost;Port=5432;User ID=postgres;Password=Pass2020!;Database=timelapse"
+dotnet user-secrets --project timelapse.api set "SendgridAPIKey" $SendgridAPIKey
