de1ca8812d6ee335eeca5cb86c1c1b97311a3e99
Leigh Hunt <leigh@venari.co.nz>
2023-03-14 14:55:49 +1300

    In progress

---
+++ timelapse.api/Pages/DeviceEdit.cshtml.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using timelapse.core.models;
+using timelapse.infrastructure;
+using Microsoft.EntityFrameworkCore;
+using timelapse.api.Helpers;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace timelapse.api.Pages
+{
+#pragma warning disable CS8618
+#pragma warning disable CS8602
+
+    [Authorize]
+    public class DeviceEditModel : PageModel
+    {
+        private readonly ILogger<DeviceEditModel> _logger;
+        private AppDbContext _appDbContext;
+        private StorageHelper _storageHelper;
+
+        public List<Device> devices {get;}
+
+        public DeviceEditModel(ILogger<DeviceEditModel> logger, AppDbContext appDbContext, IConfiguration configuration)
+        {
+            _logger = logger;
+            _appDbContext = appDbContext;
+            devices = _appDbContext.Devices
+                // .Include(d => d.Telemetries)
+                // .Include(d => d.Images)
+                // .AsSplitQuery()
+                .ToList();
+        }
+ 
+        public IActionResult OnGet(int id)
+        {
+            Device = _appDbContext.Devices.FirstOrDefault(d => d.Id == id);
+            if (Device == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        [BindProperty]
+        public Device Device { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var device = _appDbContext.Devices.FirstOrDefault(d => d.Id == id);
+
+            device.Name = Device.Name;
+            device.Description = Device.Description;
+
+            _appDbContext.Devices.Update(device);
+            await _appDbContext.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+//#pragma warning restore CS8618
+//#pragma warning restore CS8602
+}
--- timelapse.core/Models/Telemetry.cs
+++ timelapse.core/Models/Telemetry.cs
@@ -13,6 +13,7 @@ public class Telemetry
     public int BatteryPercent {get; set;}
     
     public string? Status {get; set;}
+
     public int? DiskSpaceFree {get; set;}
     public int? UptimeSeconds {get; set;}
     public int? PendingImages {get; set;}
@@ -23,4 +24,47 @@ public class Telemetry
     [Required]
     public int DeviceId {get; set;}
     public Device Device {get; set;}
+
+    public string FixUpInvalidPiJuiceJSONStatus {get {
+        if(Status!=null){
+            var status = Status;
+            status = status.Replace("'", "\"");
+            status = status.Replace(": False", ": \"False\"");
+            status = status.Replace(": True", ": \"True\"");
+            return status;
+        }
+        return null;
+    }}
+
+    public string? Status_Battery {
+        get{
+            if(Status!=null){
+                dynamic status = System.Text.Json.JsonSerializer.Deserialize<dynamic>(FixUpInvalidPiJuiceJSONStatus);
+                dynamic status2 = System.Text.Json.JsonSerializer.Deserialize<dynamic>(status.GetProperty("status"));
+                return status2.GetProperty("battery").ToString()
+                    .Replace("CHARGING_FROM_IN", "Charging")
+                    .Replace("CHARGING_FROM_5V_IO", "Charging")
+                    .Replace("NOT_PRESENT", "Not Present")
+                    .Replace("NORMAL", "Normal");
+            }
+
+            return null;
+        }
+    }
+
+    public string? Status_PowerInput {
+        get{
+            if(Status!=null){
+                dynamic status = System.Text.Json.JsonSerializer.Deserialize<dynamic>(FixUpInvalidPiJuiceJSONStatus);
+                dynamic status2 = System.Text.Json.JsonSerializer.Deserialize<dynamic>(status.GetProperty("status"));
+                return status2.GetProperty("powerInput").ToString()
+                    .Replace("WEAK", "Weak")
+                    .Replace("BAD", "Bad")
+                    .Replace("NOT_PRESENT", "Not Present")
+                    .Replace("PRESENT", "Present");
+            }
+
+            return null;
+        }
+    }
 }
