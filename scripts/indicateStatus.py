ad14ea7788f11f2e0988a71005207143c3e72f8a
Leigh Hunt <leigh@venari.co.nz>
2023-05-26 10:37:35 +1200

    Indicate status - fixing permissions stuff

--- scripts/indicateStatus.py
+++ scripts/indicateStatus.py
@@ -1,63 +1,8 @@
-import subprocess
-import json
 import pijuice
-import os
 import time
-import shutil
-import datetime
-import sys
-import requests
-import logging
-from logging.handlers import TimedRotatingFileHandler
-import glob
-import pathlib
 import socket
 
-config = json.load(open('config.json'))
-logFilePath = config["logFilePath"]
-os.makedirs(os.path.dirname(logFilePath), exist_ok=True)
-
-formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
-handler = TimedRotatingFileHandler(logFilePath, 
-                                   when='midnight',
-                                   backupCount=10)
-handler.setFormatter(formatter)
-logger = logging.getLogger("indicateStatus")
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
-
-logger.info("Starting up indicateStatus.py...")
-
-# outputImageFolder = '../output/images/'
-# pendingImageFolder = outputImageFolder + 'pending/'
-# uploadedImageFolder = outputImageFolder + 'uploaded/'
-
-# outputTelemetryFolder = '../output/telemetry/'
-# pendingTelemetryFolder = outputTelemetryFolder + 'pending/'
-# uploadedTelemetryFolder = outputTelemetryFolder + 'uploaded/'
-# holdTelemetryFolder = outputTelemetryFolder + 'hold/'
-
-# bInSupportWindow = False
-
-# def getSerialNumber():
-#   # Extract serial from cpuinfo file
-#   cpuserial = "0000000000000000"
-#   try:
-#     f = open('/proc/cpuinfo','r')
-#     for line in f:
-#       if line[0:6]=='Serial':
-#         cpuserial = line[10:26]
-#     f.close()
-#   except:
-#     cpuserial = "ERROR000000000"
-
-#   return cpuserial
-
-# serialNumber = getSerialNumber()
-
-# time.sleep(10)
 pj = pijuice.PiJuice(1, 0x14)
-# logger.info("Starting up uploadPending.py 2...")
 
 def flashLED(led='D2', R=0, G=0, B=255, flashCount=3, flashDelay=0.5):
     for i in range(0, flashCount):
@@ -75,20 +20,13 @@ def indicateStatus():
     cycleLEDs()
     flashLED('D2', 0, 0, 255, 3, 0.1)
 
-    logger.info('Indicating status...')
-
     # Green - internet OK, red no internet
     time.sleep(3)
     if internet():
-        logger.info('Internet OK')
         flashLED('D2', 0, 255, 0, 1, 2)
     else:
-        logger.info('No Internet')
         flashLED('D2', 255, 0, 0, 1, 2)
     
-
-    logger.info('Status indicated.')
-
     # Flash to indicate end of status
     flashLED('D2', 0, 0, 255, 3, 0.5)
 
@@ -105,150 +43,6 @@ def internet(host="8.8.8.8", port=53, timeout=3):
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
         return True
     except socket.error as ex:
-        logger.warning(e)
         return False
 
-# def turnOnSystemPowerSwitch(retries = 3):
-#     try:
-#         sysVoltage = pj.status.GetBatteryVoltage()['data']
-#         # if sysVoltage < 3.2:  # 3.2V is the minimum voltage for the XL6009
-#         #     logger.info('Battery voltage too low for XL6009 - not powering up modem.')
-#         #     return
-#         if sysVoltage < 3.0:  # 3.0V is a bit on the low side
-#             logger.info('Battery voltage too low - not powering up modem.')
-#             return
-#         logger.info('System Voltage looks good at ' + str(sysVoltage) + 'mV')
-
-#         modemPower = config['modem.power']
-#         if modemPower <= 0:
-#             logger.info('Modem power is disabled in config.')
-#             return
-        
-#         logger.info('Current System Power Switch:')
-#         logger.info(pj.power.GetSystemPowerSwitch())
-#         logger.info('Setting System Power Switch to ' + str(modemPower) + ':')
-#         pj.power.SetSystemPowerSwitch(modemPower)
-
-#         logger.info('Waiting 50s for modem to warm up...')
-#         time.sleep(50)
-
-#         logger.info('Waiting for network....')
-#         # Call Internet function to wait for network, for a max of 1 minute
-#         waitCounter = 0
-#         while not internet() and waitCounter < 60:
-#             time.sleep(10)
-#             logger.info('Still waiting for network....')
-#             waitCounter=waitCounter+1
-        
-#         if waitCounter < 120:
-#             logger.info('Network connection established.')
-       
-#         else:
-#             logger.warning('Could not establish network connection after 2 minutes.')
-#             if retries > 0:
-#                 logger.info('Retrying to establish network connection...')
-#                 turnOnSystemPowerSwitch(retries-1)
-#             else:
-#                 logger.warning('Retries exhausted - giving up.')
-#                 logger.info('Setting System Power Switch to Off')
-#                 pj.power.SetSystemPowerSwitch(0)
-#                 return
-    
-#     except Exception as e:
-#         logger.error(str(datetime.datetime.now()) + " turnOnSystemPowerSwitch() failed.")
-#         logger.error(e)
-
-# def uploadPendingTelemetry():
-
-#     try:
-#         os.makedirs(pendingTelemetryFolder, exist_ok = True)
-#         os.makedirs(uploadedTelemetryFolder, exist_ok = True)
-#         os.makedirs(holdTelemetryFolder, exist_ok = True)
-#         #requests.post(config['apiUrl'] + '/Telemetry', json=api_data)
-#         session = requests.Session()
-
-#         mostRecentTelemetryFiles = sorted(glob.iglob(pendingTelemetryFolder + "/*.json"), key=os.path.getctime, reverse=True)
-
-#         pendingFilesProcessed=0
-#         lastAttemptedFilename = ''
-#         for telemetryFilename in mostRecentTelemetryFiles:
-            
-#             lastAttemptedFilename = telemetryFilename
-#             # Process in batches of 100:
-#             pendingFilesProcessed+=1
-#             if pendingFilesProcessed > 100:
-#                 break
-
-#             logger.info(' uploading ' + telemetryFilename)
-
-#             telemetryTimestamp = datetime.datetime.strptime(pathlib.Path(telemetryFilename).stem, '%Y-%m-%d_%H%M%S')
-#             logger.debug('telemetryTimestamp:')
-#             logger.debug(telemetryTimestamp)
-
-#             if os.stat(telemetryFilename).st_size == 0:
-#                 os.remove(telemetryFilename)
-#                 # empty file, will throw JSONDecodeError
-#                 continue
-
-#             api_data = json.load(open(telemetryFilename, 'rb'))
-
-#             api_data['Timestamp'] = telemetryTimestamp.astimezone().isoformat()
-
-#             logger.debug(api_data)
-
-#             postResponse = session.post(config['apiUrl'] + 'Telemetry',data=api_data, timeout=5)
-#             logger.debug(postResponse)
-#             assert postResponse.status_code == 200, "API returned error code"
-#             #requests.post(config['apiUrl'] + '/Telemetry', json=api_data)
-
-#             if postResponse.status_code == 200:
-#                 logger.debug(f'Telemetry uploaded successfully')
-#                 shutil.move(telemetryFilename, uploadedTelemetryFolder + pathlib.Path(telemetryFilename).name)
-#                 logger.debug('Logged to API.')
-
-#     except requests.exceptions.ConnectionError as e:
-#         logger.error(str(datetime.datetime.now()) + " uploadPendingTelemetry() failed - connection error. Leave in place.")
-#         logger.error(e)
-#     except Exception as e:
-#         logger.error(str(datetime.datetime.now()) + " uploadPendingTelemetry() failed.")
-#         logger.error(e)
-#         if lastAttemptedFilename!="":          
-#           logger.error("lastAttemptedFilename: " + lastAttemptedFilename)
-#           # os.remove(telemetryFilename)
-#           shutil.move(lastAttemptedFilename, holdTelemetryFolder + pathlib.Path(lastAttemptedFilename).name)
-#           logger.info("Moved " + lastAttemptedFilename + " to " + holdTelemetryFolder)
-
-# def deleteOldUploadedImagesAndTelemetry():
-
-#     try:
-#       now = time.time()
-#       os.makedirs(uploadedImageFolder, exist_ok = True)
-
-#       for uploadedImageFilename in os.listdir(uploadedImageFolder):
-#         uploadedImageFilename = os.path.join(uploadedImageFolder, uploadedImageFilename)
-#         if os.stat(uploadedImageFilename).st_mtime < now - 1 * 86400:
-#           if os.path.isfile(uploadedImageFilename):
-#             logger.info(' deleting old uploaded image ' + uploadedImageFilename)
-#             os.remove(uploadedImageFilename)
-
-#       os.makedirs(uploadedTelemetryFolder, exist_ok = True)
-
-#       for uploadedTelemetryFilename in os.listdir(uploadedTelemetryFolder):
-#         uploadedTelemetryFilename = os.path.join(uploadedTelemetryFolder, uploadedTelemetryFilename)
-#         if os.stat(uploadedTelemetryFilename).st_mtime < now - 1 * 86400:
-#           if os.path.isfile(uploadedTelemetryFilename):
-#             logger.info(' deleting old uploaded telemetry ' + uploadedTelemetryFilename)
-#             os.remove(uploadedTelemetryFilename)
-
-#     except Exception as e:
-#         logger.error(str(datetime.datetime.now()) + " deleteOldUploadedImagesAndTelemetry() failed.")
-#         logger.error(e)
-
-
-try:
-    
-    config = json.load(open('config.json'))
-    indicateStatus()
-
-except Exception as e:
-    logger.error(e)
+indicateStatus()
--- scripts/indicateStatus.sh
+++ scripts/indicateStatus.sh
@@ -1,8 +1,7 @@
+#!/bin/bash
 pushd /home/pi/dev/timelapse/scripts
 set -x
 
-echo `date` >> indicateStatus.sh.out
-echo "Starting indicateStatus..." >> indicateStatus.sh.out
 /usr/bin/python3 indicateStatus.py
 
 popd
