f8d40e9481e4f91b9cea762e1293ba19f3c5aaaa
Leigh Hunt <leigh@venari.co.nz>
2023-06-16 11:59:07 +1200

    Install and update scripts

--- update.sh
+++ update.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-echo Updating....
-sudo apt-get update
-
-echo Upgrading...
-sudo apt-get upgrade -y
-
-echo Installing...
-sudo apt-get install git pijuice-base python3-pip -y
-sudo apt install -y python3-picamera2 --no-install-recommends
-sudo apt-get install vim byobu -y
-
-byobu-enable
-
-echo Setting timezone...
-sudo timedatectl set-timezone Pacific/Auckland
-
-
 echo Cloning repo...
 # Check if dev folder exists
 if [ ! -d "/home/pi/dev/timelapse" ]; then
@@ -28,7 +11,7 @@ if [ ! -d "/home/pi/dev/timelapse" ]; then
     git checkout development
 else
     cd dev/timelapse
-    git checkout development
+    # git checkout development
     git pull
 fi
 
@@ -49,21 +32,21 @@ sudo mv /var/lib/pijuice/pijuice_config.JSON /var/lib/pijuice/pijuice_config.JSO
 sudo curl -fsSL -o /var/lib/pijuice/pijuice_config.JSON https://raw.githubusercontent.com/venari/timelapse/main/pijuice_config.JSON
 sudo chown pijuice:pijuice /var/lib/pijuice/pijuice_config.JSON
 
-echo Installing Tailscale...
-curl -fsSL https://tailscale.com/install.sh | sh
-sudo tailscale up
+# echo Installing Tailscale...
+# curl -fsSL https://tailscale.com/install.sh | sh
+# sudo tailscale up
 
-# Query user for hostname, provide a default value
-read -p "Current hostname is $(hostname) - would you like to change it?" yn
-case $yn in 
-    [Yy]* ) echo "Changing hostname";
-        read -p "Enter new hostname if desired: " -i sediment-pi- -e hostname
-        echo Setting hostname to $hostname
-        sudo hostnamectl set-hostname $hostname;;
+# # Query user for hostname, provide a default value
+# read -p "Current hostname is $(hostname) - would you like to change it?" yn
+# case $yn in 
+#     [Yy]* ) echo "Changing hostname";
+#         read -p "Enter new hostname if desired: " -i sediment-pi- -e hostname
+#         echo Setting hostname to $hostname
+#         sudo hostnamectl set-hostname $hostname;;
 
-    [Nn]* ) echo "Skipping hostname change";;
-    * ) echo "Please answer yes or no.";;
-esac
+#     [Nn]* ) echo "Skipping hostname change";;
+#     * ) echo "Please answer yes or no.";;
+# esac
 
 echo We need to reboot to kick off cron jobs
 echo "Press any key to reboot"
