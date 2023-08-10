a9c6da2f07e1f070f53e88ed6837d79f60cc84d4
Leigh Hunt <leigh@venari.co.nz>
2023-08-11 11:00:52 +1200

    Event edit, plus last edited date in progress...

---
+++ timelapse.api/Migrations/20230810225358_EventLastEditedBy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace timelapse.api.Migrations
+{
+    public partial class EventLastEditedBy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "last_edited_by_user_id",
+                table: "events",
+                type: "text",
+                nullable: false,
+                defaultValue:  );
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "last_edited_date",
+                table: "events",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "last_edited_by_user_id",
+                table: "events");
+
+            migrationBuilder.DropColumn(
+                name: "last_edited_date",
+                table: "events");
+        }
+    }
+}
