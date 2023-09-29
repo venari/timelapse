1b970e82c3c57624a648f0fefd5236446eb7ef73
Leigh Hunt <leigh@venari.co.nz>
2023-09-29 13:52:41 +1300

    handleSMS timing on reboot

--- install_sedicam.sh
+++ install_sedicam.sh
@@ -66,8 +66,8 @@ echo Installing crontab entries...
 # (crontab -l 2>/dev/null; echo "@reboot /usr/bin/bash /home/pi/dev/timelapse/scripts/startup.sh")| crontab -
 (echo "@reboot /usr/bin/bash /home/pi/dev/timelapse/scripts/saveTelemetry.sh")| crontab -
 (crontab -l 2>/dev/null; echo "@reboot /usr/bin/bash /home/pi/dev/timelapse/scripts/savePhotos.sh")| crontab -
-# (crontab -l 2>/dev/null; echo "@reboot sleep 60 && /usr/bin/bash /home/pi/dev/timelapse/scripts/uploadPending.sh")| crontab -
 (crontab -l 2>/dev/null; echo "@reboot /usr/bin/bash /home/pi/dev/timelapse/scripts/uploadPending.sh")| crontab -
+(crontab -l 2>/dev/null; echo "@reboot sleep 120 && /usr/bin/bash /home/pi/dev/timelapse/scripts/handleSMS.sh")| crontab -
 (crontab -l 2>/dev/null; echo "*/5 * * * * /usr/bin/bash /home/pi/dev/timelapse/scripts/handleSMS.sh")| crontab -
 
 echo Overwriting pijuice config...
--- update_sedicam.sh
+++ update_sedicam.sh
@@ -27,6 +27,7 @@ echo Installing crontab entries...
 (echo "@reboot /usr/bin/bash /home/pi/dev/timelapse/scripts/saveTelemetry.sh")| crontab -
 (crontab -l 2>/dev/null; echo "@reboot /usr/bin/bash /home/pi/dev/timelapse/scripts/savePhotos.sh")| crontab -
 (crontab -l 2>/dev/null; echo "@reboot /usr/bin/bash /home/pi/dev/timelapse/scripts/uploadPending.sh")| crontab -
+(crontab -l 2>/dev/null; echo "@reboot sleep 120 && /usr/bin/bash /home/pi/dev/timelapse/scripts/handleSMS.sh")| crontab -
 (crontab -l 2>/dev/null; echo "*/5 * * * * /usr/bin/bash /home/pi/dev/timelapse/scripts/handleSMS.sh")| crontab -
 
 echo Overwriting pijuice config...
