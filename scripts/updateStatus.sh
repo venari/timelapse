eb6317c38eb67510857a9309917835d56d778d51
Leigh Hunt <leigh@venari.co.nz>
2023-12-01 09:16:38 +1300

    stderr redirection in scripts

--- scripts/updateStatus.sh
+++ scripts/updateStatus.sh
@@ -2,6 +2,6 @@ pushd /home/pi/dev/timelapse/scripts
 set -x
 #echo `date` >> startup.sh.out
 #echo "Uploading telemetry and images..." >> startup.sh.out
-/usr/bin/python3 updateStatus.py
+/usr/bin/python3 updateStatus.py 2>> startup.sh.out
 
 popd
