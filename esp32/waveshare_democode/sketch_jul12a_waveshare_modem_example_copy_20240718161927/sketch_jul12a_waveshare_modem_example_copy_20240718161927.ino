dfd211e2622894f375397c69cc0dce1773f566d9
Leigh Hunt <leigh@venari.co.nz>
2024-07-19 13:12:39 +1200

    Seems to be able to access internet now.

--- esp32/waveshare_democode/sketch_jul12a_waveshare_modem_example_copy_20240718161927/sketch_jul12a_waveshare_modem_example_copy_20240718161927.ino
+++ esp32/waveshare_democode/sketch_jul12a_waveshare_modem_example_copy_20240718161927/sketch_jul12a_waveshare_modem_example_copy_20240718161927.ino
@@ -13,6 +13,19 @@ static const uint32_t GPSBaud = 115200;
 
 
 
+// Your SIM card credentials
+const char apn[] = "vodafone";  // Set your APN
+const char user[] = "";         // Set your username, if any
+const char pass[] = "";         // Set your password, if any
+
+// Server details
+const char server[] = "httpbin.org"; // Server URL
+const int port = 80;                 // Server port
+
+// Initialize the TinyGSM modem
+TinyGsm modem(SerialAT);
+TinyGsmClient client(modem);
+
 String rev;
 
 void SentSerial(const char *p_char) {
@@ -47,26 +60,92 @@ void setup() {
   SerialMon.begin(115200);
   SerialAT.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin);
 
+
   while (!SentMessage("AT", 2000)) {
     delay(1000);
   }
   
-  SentMessage("ATD10086;", 2000);
-  SentSerial("ATE1;");
-  SentSerial("AT+COPS?");
-  //  14:46:03.547 -> +COPS: 0,2,"53001",7
-  SentSerial("AT+CGDCONT?");
-  // 14:46:03.580 -> +CGDCONT: 1,"IP","vodafone","100.70.101.247",,,,,,,,,,,
-  SentSerial("AT+SIMCOMATI");
-  // 14:46:03.580 -> Manufacturer: SIMCOM INCORPORATED
-  // 14:46:03.580 -> Model: SIM7670G-MNGV
-  // 14:46:03.580 -> Revision: 2360B01SIM767XM5A_M
-  // 14:46:03.580 -> SIM767XM5A_B01V03_231207
-  // 14:46:03.580 -> IMEI: 864643060060052
-  // 14:46:03.580 -> 
+  // SentSerial("ATE1;");
+  // SentSerial("AT+COPS?");
+  // // //  14:46:03.547 -> +COPS: 0,2,"53001",7
+  // SentSerial("AT+CGDCONT?");
+  // // // 14:46:03.580 -> +CGDCONT: 1,"IP","vodafone","100.70.101.247",,,,,,,,,,,
+  // SentSerial("AT+SIMCOMATI");
+  // // 14:46:03.580 -> Manufacturer: SIMCOM INCORPORATED
+  // // 14:46:03.580 -> Model: SIM7670G-MNGV
+  // // 14:46:03.580 -> Revision: 2360B01SIM767XM5A_M
+  // // 14:46:03.580 -> SIM767XM5A_B01V03_231207
+  // // 14:46:03.580 -> IMEI: 864643060060052
+  // // 14:46:03.580 -> 
+
+  // This seems to sometimes be necessary - e.g. after removing SIM card. Maybe optimise later if we can?
+  SerialMon.println("Initializing modem...");
+  modem.restart();
+  
+  
+  // return;
+
+  // This bit always hangs for a minute and then fails...
+  
+  // SerialMon.print("Connecting to network...");
+  // if (!modem.waitForNetwork()) {
+  //   SerialMon.println(" fail");
+  //   // while (true);
+  //   return;
+  // }
+  // SerialMon.println(" success");
+
+  // GPRS connection
+  SerialMon.print(F("Connecting to "));
+  SerialMon.print(apn);
+  if (!modem.gprsConnect(apn, user, pass)) {
+    SerialMon.println(" fail");
+    // while (true);
+    return;
+  }
+  SerialMon.println(" success");
+
+  // Make HTTP GET request
+  if (!client.connect(server, port)) {
+    SerialMon.println("Connection to server failed");
+    return;
+  }
+
+  // Make a HTTP GET request
+  client.print(String("GET /get HTTP/1.1\r\n") +
+               "Host: " + server + "\r\n" +
+               "Connection: close\r\n\r\n");
+
+  // Wait for server response
+  unsigned long timeout = millis();
+  while (client.connected() && millis() - timeout < 10000) {
+    while (client.available()) {
+      String line = client.readStringUntil('\r');
+      SerialMon.print(line);
+      timeout = millis();
+    }
+  }
+
+  // Close the connection
+  client.stop();
+  SerialMon.println(F("Server disconnected"));
+
+
+
+
 }
 
+static const unsigned long REFRESH_INTERVAL = 5000; // ms
+static unsigned long lastRefreshTime = 0;
+
 void loop() {
+
+  if(millis() - lastRefreshTime >= REFRESH_INTERVAL)
+  {
+  	lastRefreshTime += REFRESH_INTERVAL;
+    Serial.print(".");
+  }
+
   if (SerialAT.available()) {
     rev = SerialAT.readString();
     SerialMon.println(rev);
