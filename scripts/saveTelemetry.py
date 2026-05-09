6e4eff08b416cc9aa0ddc3b9cf2fe233a0d55790
Leigh Hunt <leigh.hunt@lynker-analytics.com>
2026-05-09 14:47:23 +1200

    Tweaking mcu clock

--- scripts/saveTelemetry.py
+++ scripts/saveTelemetry.py
@@ -433,17 +433,19 @@ def saveTelemetry():
 
 if pj_is_alive():
     try:
-        logger.debug('PvPi is alive, determining which clock is further in the future')
+        logger.info('PvPi is alive, determining which clock is further in the future')
         mcu_time = pvpiClient.get_mcu_time()
+        logger.info('pvpi MCU time: ' + str(mcu_time))
+        logger.info('system time: ' + str(datetime.datetime.now()))
         if(mcu_time > datetime.datetime.now()):
-            logger.debug('setting sys clock from pvpi MCU time...')
+            logger.info('setting sys clock from pvpi MCU time...')
             if mcu_time.year <= 2025:
                 logger.warning("MCU time looks wrong, so we're not setting system clock from it.")
                 loggerIntent.warning("MCU time looks wrong, so we're not setting system clock from it.")
             else:
                 subprocess.call(['sudo', 'date', '-s', mcu_time.strftime('%Y-%m-%d %H:%M:%S')])
         else:
-            logger.debug('pvpi MCU time is behind system time, so we will set mcu from sys clock.')
+            logger.info('pvpi MCU time is behind system time, so we will set mcu from sys clock.')
             pvpiClient.set_mcu_time(datetime.datetime.now())
 
     except Exception as e:
