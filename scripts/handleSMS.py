cbec7a800d1d6d03260c9d387a27c66b8d57b6a4
Leigh Hunt <leigh.hunt@lynker-analytics.com>
2026-06-02 20:01:57 +1200

    troubleshooting 4G connection

--- scripts/handleSMS.py
+++ scripts/handleSMS.py
@@ -38,7 +38,8 @@ logger.debug(rec_lines)
 phone_number=''
 
 for line in rec_lines:
-    line = line.decode()
+    if isinstance(line, bytes):
+        line = line.decode()
     logger.info(line)
     if(line.startswith("+CMGL:")):
         # Header
