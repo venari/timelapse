062bf4f75473f68dcc58e3abda4b096ee1a48d31
Leigh Hunt <leigh.hunt@lynker-analytics.com>
2026-06-04 11:06:46 +1200

    Removing dependencies in detectHang - e.g. PvPi, or corrupt config.json

--- scripts/detectHang.py
+++ scripts/detectHang.py
@@ -1,6 +1,5 @@
 import subprocess
 import json
-from pvpi import PvPiClient
 import os
 import sys
 import time
@@ -11,27 +10,12 @@ import pathlib
 
 from SIM7600X import powerUpSIM7600X, powerDownSIM7600X
 
-config = json.load(open(pathlib.Path(__file__).parent / 'config.json'))
-logFilePath = config["logFilePath"]
-intentLogFilePath = logFilePath.replace("timelapse.log", "intent.log")
-os.makedirs(os.path.dirname(logFilePath), exist_ok=True)
-
 formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
 handler = logging.StreamHandler(sys.stderr)
 handler.setFormatter(formatter)
 logger = logging.getLogger("detectHang")
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
-
-handlerIntent = logging.FileHandler(intentLogFilePath)
-handlerIntent.setFormatter(formatter)
-loggerIntent = logging.getLogger("intent")
-loggerIntent.addHandler(handlerIntent)
-loggerIntent.setLevel(logging.DEBUG)
-
-# logger.info("Starting up detectHang.py...")
-# loggerIntent.info("Starting up detectHang.py...")
-# os.chmod(logFilePath, 0o777) # Make sure pijuice user script can write to log file.
 
 outputImageFolder = str(pathlib.Path(__file__).parent / '../output/images/')
 workingImageFolder = os.path.join(outputImageFolder , 'working/')
@@ -42,30 +26,12 @@ outputTelemetryFolder = str(pathlib.Path(__file__).parent / '../output/telemetry
 pendingTelemetryFolder = os.path.join(outputTelemetryFolder , 'pending/')
 uploadedTelemetryFolder = os.path.join(outputTelemetryFolder , 'uploaded/')
 
-# pvpi
-time.sleep(10)
-pvpiClient = None
-try:
-    pvpiClient = PvPiClient()
-except:
-    logger.error("PvPi not connected - PvPi functionality will not be available")
-
-def pj_is_alive():
-    try:
-        return pvpiClient is not None and pvpiClient.get_alive()
-    except Exception:
-        return False
-
 def detectHang():
     try:
-
-        config = json.load(open(pathlib.Path(__file__).parent / 'config.json'))
 
         hung = False
 
         uptimeSeconds = int(time.clock_gettime(time.CLOCK_BOOTTIME))
-
-
 
         # If we've been up for more than 45 minutes, and the most recently captured image is older than 10 minutes, or the most recently uploaded image is older than 30 minutes, 
         # either network is out, or we can't get a cellular signal, DNS is messing around, or camera isn't capturing, and the saveTelemetry script hasn't caught it - it may have hung.
@@ -95,33 +61,24 @@ def detectHang():
             # Most recent image captured (may also be in uploaded folder) is older than 10 minutes
             if secondsSinceLastImageCapture > 600 and secondsSinceLastUpload > 600:
                 logger.warning('detectHang - Most recent captured image is ' + str(secondsSinceLastImageCapture) + 'seconds old, and uploaded image is ' + str(secondsSinceLastUpload) + ' seconds old - restarting...')
-                loggerIntent.warning('detectHang - Most recent captured image is ' + str(secondsSinceLastImageCapture) + 'seconds old, and uploaded image is ' + str(secondsSinceLastUpload) + ' seconds old - restarting...')
                 hung = True
 
             if secondsSinceLastUpload > 1800:
                 logger.warning('detectHang - Most recent uploaded image is ' + str(secondsSinceLastUpload) + ' seconds old - restarting...')
-                loggerIntent.warning('detectHang - Most recent uploaded image is ' + str(secondsSinceLastUpload) + ' seconds old - restarting...')
                 hung = True
 
             if len(mostRecentPendingFiles) == 0 and len(mostRecentUploadedFiles) == 0:
                 logger.debug("detectHang - No uploaded or captured images found - restarting...")
-                loggerIntent.debug("detectHang - No uploaded or captured images found - restarting...")
                 hung = True
 
 
         if hung == True:
             logger.info("detectHang - we're bouncing...")
-            loggerIntent.info("detectHang - we're bouncing...")
 
-            if pj_is_alive():
-                logger.debug('get_mcu_time(): ' + str(pvpiClient.get_mcu_time()))
-                loggerIntent.debug('get_mcu_time(): ' + str(pvpiClient.get_mcu_time()))
-        
             # logger.info('Setting System Power Switch to Off:')
             # pj.power.SetSystemPowerSwitch(0)
             powerDownSIM7600X()
             logger.info('detectHang - Restarting after hang now...')
-            loggerIntent.info('detectHang - Restarting after hang now...')
             subprocess.call(['sudo', 'shutdown', '-r', 'now'])
 
     except Exception as e:
